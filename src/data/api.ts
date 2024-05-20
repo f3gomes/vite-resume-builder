ef1be0961f2de548f565b73faf020a6710396fc8
fomes <fgomesdeluna@gmail.com>
2024-05-20 15:56:46 -0300

    fix: update api request

--- src/data/api.ts
+++ src/data/api.ts
@@ -6,11 +6,19 @@ export const resumeApi = axios.create({
 });
 
 export const updateUser = async (userId: string, payload: object) => {
-  const response = await resumeApi.put(`/users/${userId}`, payload);
-  return response.data;
+  try {
+    const response = await resumeApi.put(`/users/${userId}`, payload);
+    return response.data;
+  } catch (error) {
+    console.log("Erro ao atualizar usuário: ", error);
+  }
 };
 
 export const createUser = async (payload: object) => {
-  const response = await resumeApi.post(`/users`, payload);
-  return response.data;
+  try {
+    const response = await resumeApi.post(`/users`, payload);
+    return response.data;
+  } catch (error) {
+    console.log("Erro ao criar usuário: ", error);
+  }
 };
